bd52ce061588a551a3d2cf3cb5e708b10fb6efc3
wylken <wylken.ufal@gmail.com>
2022-07-09 21:55:25 -0300

    adding styled-components and Dashboard page

--- App.tsx
+++ App.tsx
@@ -1,21 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {ThemeProvider} from 'styled-components'
+
+import { Dashboard } from './src/pages/Dashboard/';
+import theme from './src/global/styles/theme'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>App W-Poll</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <Dashboard/>
+    </ThemeProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+import {Container} from "./styles"
+
+export function Dashboard(){
+    return(
+        <Container>
+            <Text>Dashboard 4</Text>
+        </Container>
+    )
+}
+
